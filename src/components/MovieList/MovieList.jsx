cf636d7fddf11d764baa0a2981d5142248f416a7
Yuriy Dobryanskiy <dobryansky@i.ua>
2024-04-01 03:11:41 +0300

    add

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,13 +1,18 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import styles from './MovieList.module.css';
 
 const MoviesList = ({ trendingMoviesList }) => {
+  const location = useLocation();
   return (
     <ul className={styles.list}>
       {trendingMoviesList.map(({ id: movieId, title }) => {
         return (
           <li key={movieId} className={styles.listItem}>
-            <NavLink to={`/movies/${movieId}`} className={styles.listLink}>
+            <NavLink
+              to={`/movies/${movieId}`}
+              className={styles.listLink}
+              state={location}
+            >
               <p>{title}</p>
             </NavLink>
           </li>
